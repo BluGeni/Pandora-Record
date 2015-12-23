c78ce6e7b75b4ce31dac0536cf126bdd51cc6b0e
Carter <cmminear@gmail.com>
2015-12-23 12:48:13 -0600

    changed config file to json format

--- pandora-recorder.py
+++ pandora-recorder.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
-import subprocess, time, os, sys
+import subprocess
+import time
+import os
+import sys
+import warnings
+import json
+import argparse
+from os.path import expanduser
 
 # Requires Pulse audio to be in use
 
-# record-pandora.conf - config file. LINE 1: Audio source | LINE 2 - min output file size per track, anything smaller is deleted | LINE 3 - output format, ogg or mp3
+# record-pandora.conf - config file. LINE 1: Audio source | LINE 2 - min output file size per track, anything smaller is deleted | LINE 3 - output format, ogg or mp3 | LINE 4 - output folder name, no / needed at the end
 # To get the parm for line 1 do this at the command line: pactl list sources
 
 # EXAMPLE CONFIG FILE: 
@@ -11,6 +18,7 @@ import subprocess, time, os, sys
 # alsa_output.pci-0000_00_1b.0.analog-stereo.monitor
 # 800
 # mp3
+# Music
 
 
 # whitelist-artists.txt -- a list of artists to record, one per line, case sensitive, names must match Pandora name output exactly
@@ -21,11 +29,6 @@ import subprocess, time, os, sys
 def startRecord(playingInfo1, playingInfo2, playingInfo3):
 
     try: #try to create new artist dir if not already there
-        #remove illegal character / for file/folder names
-        #playingInfo1 = playingInfo1.replace("/", "-")
-        #playingInfo2 = playingInfo2.replace("/", "-")
-        #playingInfo3 = playingInfo3.replace("/", "-")
-        #above moved
         global dontDelete
         if(not os.path.exists(folderName + "/" + playingInfo1)):
             os.mkdir(folderName + "/" + playingInfo1)
@@ -60,6 +63,16 @@ def startRecord(playingInfo1, playingInfo2, playingInfo3):
 
 ### program start here ###
 
+
+#construct the argument parser and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-c", "--conf", required=True,
+#     help="path to the JSON configuration file")
+# args = vars(ap.parse_args())
+ 
+
+
+
 #variables and lists
 p2 = ""
 dontDelete = False
@@ -68,18 +81,29 @@ dontDelete = False
     
 #read configuration file
 try:
-    if(os.path.exists("record-pandora.conf")):
-	print "Loading config file...\n"
-	confFile = open("record-pandora.conf", "r")
-        audioDevName = confFile.readline()
-        audioDevName = audioDevName.rstrip("\n")
-        delFileSize = confFile.readline()
-        delFileSize = delFileSize.rstrip("\n")
-        outputType = confFile.readline()
-        outputType = outputType.rstrip("\n")
-        folderName = confFile.readline()
-        folderName = folderName.rstrip("\n")        
-        confFile.close()
+    if(os.path.exists("config.json")):
+    	print "Loading config file...\n"
+        #filter warnings, load the configuration
+        warnings.filterwarnings("ignore")
+        conf = json.load(open("config.json"))
+
+        #read the config.json values
+        audioDevName = conf["audioDevice"]
+        delFileSize = int(conf["deleteFilesUnder"])
+        outputType = conf["saveToFormat"]
+        folderName = conf["saveToDir"]
+
+
+	# confFile = open("record-pandora.conf", "r")
+ #        audioDevName = confFile.readline()
+ #        audioDevName = audioDevName.rstrip("\n")
+ #        delFileSize = confFile.readline()
+ #        delFileSize = delFileSize.rstrip("\n")
+ #        outputType = confFile.readline()
+ #        outputType = outputType.rstrip("\n")
+ #        folderName = confFile.readline()
+ #        folderName = folderName.rstrip("\n")        
+ #        confFile.close()
         if(outputType == ""):
 		outputType = "ogg"
 	elif(outputType != "ogg" and outputType != "mp3"):
